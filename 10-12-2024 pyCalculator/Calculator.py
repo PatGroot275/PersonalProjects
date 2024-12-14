9ba69d26c4478f4c1943e1709a5cea533c41e2bc
Pat <days_utensil_0q@icloud.com>
2024-12-14 14:43:40 -0800

    Add files via upload

---
+++ 10-12-2024 pyCalculator/Calculator.py
@@ -0,0 +1,7 @@
+input("Enter an operation:")
+operations = ['+', '-', '*', '/']
+
+def intInputArray(message, inputArray):
+    return "blah"
+def stringInputArray(message, inputArray):
+    return "blah"
---
+++ 10-16-2024 pyBasicDocker/app.py
@@ -0,0 +1,4 @@
+x = int(input("Please enter a number: \n"))
+print("You entered...")
+for i in range(1,x):
+    print(i)
